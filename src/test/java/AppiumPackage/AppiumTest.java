ec041678b0aa7d758ceeec638e2727bad692b537
duceldorf <39568719+duceldorf@users.noreply.github.com>
2018-10-24 15:28:08 +0300

    Start2

--- src/test/java/AppiumPackage/AppiumTest.java
+++ src/test/java/AppiumPackage/AppiumTest.java
@@ -3,10 +3,11 @@ package AppiumPackage;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+
 
 public class AppiumTest {
 
@@ -15,16 +16,16 @@ public class AppiumTest {
         //Set the Desired Capabilities
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("deviceName", "My Phone");
-        caps.setCapability("udid", "ENUL6303030010"); //Give Device ID of your mobile phone
+        caps.setCapability("udid", "712HFCSD22BVC"); //Give Device ID of your mobile phone
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "6.0");
-        caps.setCapability("appPackage", "com.android.vending");
-        caps.setCapability("appActivity", "com.google.android.finsky.activities.MainActivity");
+        caps.setCapability("platformVersion", "7.0");
+        caps.setCapability("appPackage", "com.social.connect.clubinclub");
+        caps.setCapability("appActivity", "com.social.connect.clubinclub.presentation.main.MainActivity");
         caps.setCapability("noReset", "true");
 
         //Instantiate Appium Driver
         try {
-            AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+            AppiumDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
 
         } catch (MalformedURLException e) {
             System.out.println(e.getMessage());
